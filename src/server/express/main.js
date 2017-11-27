a8be09db79bd0d99bd1d70801c9f7cc01c105daf
vbouquet <valentin-bouquet@hotmail.fr>
2017-11-27 17:39:07 +0100

    Add node express server for front-end testing

---
+++ src/server/express/main.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+const app = express();
+const port = 8080;
+
+app.use('/', express.static("/vagrant/src/client"));
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, '0.0.0.0');
+console.log('Listening on port', port);
