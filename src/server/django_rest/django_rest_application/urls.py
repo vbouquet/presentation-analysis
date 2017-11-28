4f4e9035edeff274519b18e39eab4d20ad6e506b
Remy Pocquerusse <remypoc@free.fr>
2017-11-28 15:06:25 +0100

    Add file support for drf

--- src/server/django_rest/django_rest_application/urls.py
+++ src/server/django_rest/django_rest_application/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+
+from django_rest_application import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include('snippets_example.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/server/django_rest/snippets_example/views.py
+++ src/server/django_rest/snippets_example/views.py
@@ -1,51 +1,32 @@
+import os
+from django.db import models
+from django.dispatch import receiver
+from rest_framework.parsers import MultiPartParser
+
 from snippets_example.models import Snippet
 from snippets_example.serializers import SnippetSerializer
-from rest_framework import generics
-from django.contrib.auth.models import User
-from snippets_example.serializers import UserSerializer
-from rest_framework import permissions
-from snippets_example.permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view
-from rest_framework.reverse import reverse
-from rest_framework import renderers
-from rest_framework.response import Response
 from rest_framework import viewsets
-from rest_framework.decorators import detail_route
-from rest_framework.response import Response
 
 
+# ModelViewSet provide default crud operations
 class SnippetViewSet(viewsets.ModelViewSet):
     """
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
-
-    Additionally we also provide an extra `highlight` action.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly,)
-
-    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
-    def highlight(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
+    parser_classes = (MultiPartParser,)
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        serializer.save()
 
-
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    This viewset automatically provides `list` and `detail` actions.
-    """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
+    # Used to delete files on filesystem
+    @receiver(models.signals.post_delete, sender=Snippet)
+    def auto_delete_file_on_delete(sender, instance, **kwargs):
+        """
+        Deletes file from filesystem when corresponding `MediaFile` object is deleted.
+        """
+        if instance.file:
+            if os.path.isfile(instance.file.path):
+                os.remove(instance.file.path)
