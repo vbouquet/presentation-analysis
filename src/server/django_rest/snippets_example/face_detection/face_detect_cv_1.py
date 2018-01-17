0479487f24c90e5585d386a7d05656a10faaa0a2
Remy Pocquerusse <remypoc@free.fr>
2018-01-17 14:55:04 +0100

    Add test picture

---
+++ src/server/django_rest/snippets_example/face_detection/face_detect_cv_1.py
@@ -0,0 +1,34 @@
+import sys
+import cv2
+
+print(cv2.__version__)
+
+
+# Get user supplied values
+imagePath = sys.argv[1]
+cascPath = "haarcascade_frontalface_default.xml"
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=2,
+    minSize=(20, 20),
+    maxSize=(80, 80)
+    #flags = cv2.CV_HAAR_SCALE_IMAGE
+)
+
+print("Found {0} faces!".format(len(faces)))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+cv2.imwrite("faces_found_1.png", image)
