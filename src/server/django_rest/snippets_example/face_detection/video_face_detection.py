1985ed637ef12869baed105a94a7b899c80e260e
Remy Pocquerusse <remypoc@free.fr>
2018-01-30 19:01:03 +0100

    Detect faces from video stream and return attendance

--- src/server/django_rest/snippets_example/face_detection/video_face_detection.py
+++ src/server/django_rest/snippets_example/face_detection/video_face_detection.py
@@ -1,30 +1,71 @@
 import cv2
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+FRAME_POS = 0
+FIRST_FILENAME = ""
+
 
 def face_detection(filename):
     print(cv2.__version__)
 
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    face_csc = cv2.CascadeClassifier(os.path.join(BASE_DIR, 'face_detection/haarcascade_frontalface_default.xml'))
-    file = os.path.join(BASE_DIR, '../media/' + filename)
-    cam = cv2.VideoCapture(file)
+    global FRAME_POS
 
-    # Capture frame by frame
+    # //TODO move later, only need to be used the first time
+    img_folder = BASE_DIR + '/face_detection/faces_found/'
+    if not os.path.exists(img_folder):
+        os.makedirs(img_folder)
+
+    # Load face detector algorithm
+    face_csc = cv2.CascadeClassifier(os.path.join(BASE_DIR, 'face_detection/haarcascade_frontalface_default.xml'))
+
+    file = os.path.join(BASE_DIR, '../media/' + filename)
+
+    # Used to detect if stream have been reset and set frame counter to 0.
+    global FIRST_FILENAME
+    if not FIRST_FILENAME:
+        FIRST_FILENAME = file
+
+    try:
+        f1 = open(FIRST_FILENAME, 'rb')
+        f1.close()
+    except FileNotFoundError:
+        FIRST_FILENAME = file
+        FRAME_POS = 0
+
+    # Used to create dynamically the final file associated with all incoming blobs
+    # Final_file is the video file made from all video files fetched from client.
+    final_file = os.path.join(BASE_DIR, '../media/final_video.webm')
+
+    # Recompose final video with incoming blob
+    with open(final_file, 'ab') as ff:
+        f1 = open(file, 'rb')
+        ff.write(f1.read())
+
+        f1.close()
+        ff.close()
+
+    cam = cv2.VideoCapture(final_file)
+
     faces_found = []
 
+    print("FRAME_POS = %f" % FRAME_POS)
+
+    # Capture frame by frame
     while cam.isOpened():
 
         # current frame number
-        frameId = cam.get(1)
+        frame_id = cam.get(1)
         ret, frame = cam.read()
+
         if not ret:
             break
         try:
-            if frameId % 10 == 0:
-                img = "faces_found_" + str(int(frameId)) + ".png"
-
+            # Face detection every 10 frames
+            if frame_id % 60 == 0 and frame_id > FRAME_POS:
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                print("Analyze frame %d" % frame_id)
 
                 faces = face_csc.detectMultiScale(
                     gray,
@@ -36,104 +77,23 @@ def face_detection(filename):
 
                 faces_found.append(len(faces))
 
+                # Uncomment to generate frame image with detected faces.
+                # Beware it creates a lot of images !
+                img = BASE_DIR + "/face_detection/faces_found/faces_found_" + str(int(frame_id)) + ".png"
+
                 # Draw a rectangle around the faces
                 for (x, y, w, h) in faces:
                     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 5)
 
                 # Used to write image
-                # cv2.imwrite(img, frame)
+                cv2.imwrite(img, frame)
+
+                FRAME_POS = frame_id
         except ZeroDivisionError:
             pass
     cam.release()
     try:
+        print(faces_found)
         return max(faces_found)
     except ValueError:
         return 0
-
-# cv2.destroyAllWindows()
-
-
-# ###### Attempt to use opencv method to analyze video second per second #####
-#
-# if tf:
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#
-#     faces = face_csc.detectMultiScale(
-#         gray,
-#         scaleFactor=1.1,
-#         minNeighbors=5,
-#         minSize=(30, 30),
-#         # flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-#     )
-#
-#     print("Found {0} faces!".format(len(faces)))
-#
-#     # Draw a rectangle around the faces
-#     for (x, y, w, h) in faces:
-#         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 5)
-#
-#     cv2.imwrite("faces_found_1.png", frame)
-
-# cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'VP80'))
-#
-# print(cam.get(cv2.CAP_PROP_FOURCC))
-
-# # Capture frame by frame
-# tf, frame = cam.read()
-#
-# print(tf)
-#
-# if tf:
-#
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#
-#     faces = face_csc.detectMultiScale(
-#         gray,
-#         scaleFactor=1.1,
-#         minNeighbors=5,
-#         minSize=(30, 30),
-#         # flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-#     )
-#
-#     print("Found {0} faces!".format(len(faces)))
-#
-#     # Draw a rectangle around the faces
-#     for (x, y, w, h) in faces:
-#         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 5)
-#
-#     cv2.imwrite("faces_found_1.png", frame)
-#
-#     cv2.imwrite("faces_found_1.png", frame)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-# cam.set(cv2.CAP_PROP_POS_MSEC, 0)
-# print(cam.get(cv2.CAP_PROP_POS_MSEC))
-# cam.set(cv2.CAP_PROP_POS_FRAMES, 1000)
-# print(cam.get(cv2.CAP_PROP_POS_MSEC))
-# file = os.path.join(BASE_DIR, '../media/video-2018-01-28T21-36-11-871Z.webm')
-# file2 = os.path.join(BASE_DIR, '../media/video-2018-01-28T21-36-16-891Z.webm')
-#
-# cam = cv2.VideoCapture(file)
-
-
-# ###### Video encoder : create new video based on video file input ######
-# frame_width = int(cam.get(3))
-# frame_height = int(cam.get(4))
-#
-# print(frame_width, "x", frame_height)
-#
-# out = cv2.VideoWriter('outpy.avi', cv2.VideoWriter_fourcc(*'MPEG'), 20, (frame_width, frame_height))
-#
-# while cam.isOpened():
-#     ret, frame = cam.read()
-#     if ret:
-#         # write the flipped frame
-#         out.write(frame)
-#         if cv2.waitKey(1) & 0xFF == ord('q'):
-#             break
-#     else:
-#         break
-# out.release()
-
-# cam.release()
-# file = "outpy.avi"
